b68ab66a2a024053e51378bd6aeb57c258c9d635
Босс <udpro@bk.ru>
2022-05-25 21:20:41 +0300

    Списать проект

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
 
         <NavPanel />
+
         <Tecnologes />
 
     </div>
--- src/Components/NavPanel/NavPanel.js
+++ src/Components/NavPanel/NavPanel.js
@@ -3,12 +3,21 @@ import App from "../../App";
 function NavPanel() {
     return (
         <div className="Nav">
+            <div className="Logo_box">  <img width='120' src="https://turbologo.ru/create-logo/templates/78837-sushi-krug-logotip" alt="Logo"/></div>
 
-            <a href="">Главная страница </a>
-            <a href=""> О нас </a>
-            <a href=""> Контакты </a>
-            <a href="">Магазин</a>
 
+            <div className="Nav_box">
+                <a href="">Состояние станции</a>
+                <a href=""> О нас </a>
+                <a href=""> Контакты </a>
+                <a href="">Магазин</a>
+            </div>
+            <div className="Login_box">
+
+
+                <button className="button-10" role="button">Button 10</button>
+
+            </div>
         </div>
 
     );
